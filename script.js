baa24bd94d815375c63a3946a17da980e2535fba
JonasFiechter <jonasrfiechter@gmail.com>
2022-03-20 16:27:32 -0300

    Update

--- script.js
+++ script.js
@@ -12,9 +12,26 @@ document.querySelector('.guess').value = 244
 console.log(document.querySelector('.guess').value)
 */
 
+const secret_number = Math.trunc(Math.random()*20);
+let score = 20;
+document.querySelector('.number').textContent = secret_number
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(typeof guess, guess);
+    console.log(secret_number)
 
-    
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number!';
+    } else if (guess === secret_number) {
+        document.querySelector('.message').textContent = 'Correct Number!';
+    } else if (guess > secret_number) {
+        document.querySelector('.message').textContent = 'Too high!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else if (guess < secret_number) {
+        document.querySelector('.message').textContent = 'Too low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
 })
